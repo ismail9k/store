3378ce9daefa2e87bd00d290c4092f02d5b8884b
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-06-21 23:31:56 +0300

    feat: add navbar component

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+header.navbar
+  .navbar-container
+    .navbar-brand
+      | Online Store
+      h1(class="u-sr-only") Online Store
+    .navbar-start
+    .navbar-end
+      .navbar-item
+        AppButton(state="primary" :outline="true") Login
+      .navbar-item
+        AppButton(state="primary" :clean="true")
+          AppIcon(name="cart" size="large" color="priamry")
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar'
+};
+</script>
