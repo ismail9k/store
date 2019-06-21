448b8f8eb9394e4c2f9f0f886db0829098d5d0b6
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-06-21 23:27:34 +0300

    chore: fix linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
+  env: {
+    node: true
+  },
   parserOptions: {
     parser: 'babel-eslint',
     sourceType: 'module'
-  },
-  globals: {
-    module: false
   },
   plugins: ['vue', 'prettier'],
   extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:prettier/recommended'],
   rules: {
     'prettier/prettier': 'error',
-    'vue/max-attributes-per-line': 'off'
+    'vue/max-attributes-per-line': 'off',
+    'space-before-function-paren': 'off'
   }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   trailingComma: 'none',
-  printWidth: 120, // don't you dare change this
+  printWidth: 120,
   tabWidth: 2,
   semi: true,
   singleQuote: true,
