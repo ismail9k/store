2b56ff4182e6fd2e0b695b5392580b5a811d72c3
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-06-21 23:27:07 +0300

    chore: inject styles variables in each vue components

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)));
+  }
+};
+
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [path.resolve(__dirname, './src/stylus/core/_index.styl')]
+    });
+}
