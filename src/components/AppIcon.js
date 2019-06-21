0d4a801ed6437db8eaa87fd8fe0370e5485bab0a
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-06-21 23:29:29 +0300

    feat: add icon component

---
+++ src/components/AppIcon.js
@@ -0,0 +1,60 @@
+import icons from '@/assets/data/icons.json';
+
+export default {
+  name: 'AppIcon',
+  functional: true,
+  inheritAttrs: true,
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  render(h, { props, data }) {
+    let icon = icons[props.name];
+    if (!icon) {
+      return;
+    }
+
+    const styleClasses = ['icon', `is-${props.name}`, `is-${props.size}`, `is-${props.color}`, data.staticClass];
+    const children = [];
+
+    // add empty square
+    children.push(
+      h('path', {
+        attrs: {
+          d: 'M0 0h24v24H0z',
+          fill: 'none'
+        }
+      })
+    );
+    // add icon title
+    children.push(h('title', props.name));
+    children.push(
+      h('path', {
+        attrs: {
+          d: icon
+        }
+      })
+    );
+
+    return h(
+      'svg',
+      {
+        attrs: {
+          class: styleClasses.join(' '),
+          viewBox: '0 0 24 24'
+        }
+      },
+      children
+    );
+  }
+};
