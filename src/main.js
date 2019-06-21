3ee8232a97e014e0f202746bdb0c96019185a0d8
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-06-21 23:32:31 +0300

    feat: auto import all app-prefixed components

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import './stylus/index.styl';
 
 Vue.config.productionTip = false;
 
+// Import components that prefixed with App globally
+const requireComponent = require.context('./components', false, /App[\w-]+\.(?:vue|js)$/);
+requireComponent.keys().forEach(fileName => {
+  const config = requireComponent(fileName);
+  const name = config.default ? config.default.name : config.name;
+
+  Vue.component(name, config.default || config);
+});
+
 new Vue({
   store,
   render: h => h(App)
